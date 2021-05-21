3604329dcb58401226959b70523f239038ef883c
CardS <CardS@DESKTOP-GUE3R94>
2021-05-21 17:36:21 -0400

    forgot to close scanner

--- MealPicker/src/mp/MealPicker.java
+++ MealPicker/src/mp/MealPicker.java
@@ -6,9 +6,9 @@ public class MealPicker {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter number of meals you wish to generate");
+		System.out.println("Enter number of meals you wish to generate: ");
 		int numMeals = scan.nextInt();
-		
+		scan.close();
 
 	}
 
