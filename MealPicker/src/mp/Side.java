3f8af88509b12eca97dae003a2bf1e7c1c268768
Aaron Meadows <Aaron Meadows>
2021-05-21 15:41:22 -0400

    Added Side Class

--- MealPicker/src/mp/Side.java
+++ MealPicker/src/mp/Side.java
@@ -1,5 +1,36 @@
 package mp;
+import java.util.ArrayList;
 
 public class Side {
-
+	String name;
+	ArrayList<String> ingredients ;
+	
+	public Side(String name, ArrayList<String> ingred) {
+		this.name = name;
+		this.ingredients = ingred;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public ArrayList<String> getIngredients() {
+		return ingredients;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setIngredients(ArrayList<String> ingred) {
+		this.ingredients = ingred;
+	}
+	
+	public boolean addIngredient(String ingred) {
+		return ingredients.add(ingred);
+	}
+	
+	public boolean removeIngredient(String ingred) {
+		return ingredients.remove(ingred);
+	}
 }
